1aca1b9e11f1639fa7889b249222f4ee5764d399
mannyjl625 <mjl288@scarlemail.rutgers.edu>
2013-03-08 13:09:42 -0500

    login for blog post

---
+++ index.php
@@ -0,0 +1,8 @@
+<form name = "login" action= "login.php" method = "post">
+Username: <input type = "text" name = "username"/></br>
+Password: <input type = "password" name = "password"/></br>
+<input type = "submit" value = "Login" />
+</form>
+<a href = "register.php"><strong>Register</strong></a>
+
+
