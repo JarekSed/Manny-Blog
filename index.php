e08cf8a35dd5470d7c30960149f8937a95d8d8b6
mannyjl625 <mjl288@scarlemail.rutgers.edu>
2013-03-12 17:32:32 -0400

    barebones login functionality complete. on to bootstraping

--- index.php
+++ index.php
@@ -1,15 +1,38 @@
+<!DOCTYPE html>
 
+<html>
+    <head>
+        <title>Manny's Blog</title>
+    </head>
+    <body>
+            <?php
+            session_start();  // creates session array(kinda like POST) that can save  variables between pages
+            if(isset($_SESSION['username'])){
+                echo  " Hello " . $_SESSION['username']; // use "." 
+            ?>
+                <form name = "logout" action= "login.php" method = "post">
+                <input type = "submit" name = "logoutButton" value = "logout"/>
 
+            <?php
+            
+            }
+            
+            else{
+                if(isset($_SESSION['loginFail'])){
+                    echo $_SESSION['loginFail'] . "</br>";
+                    echo "login fail";
+                    unset($_SESSION['loginFail']);
+                }
+            ?>  
+                <form name = "login" action= "login.php" method = "post">
+                Username: <input type = "text" name = "username"/></br>
+                Password: <input type = "password" name = "password"/></br>
+                <input type = "submit" value = "Login" />
+                </form>
+                <a href = "register.php"><strong>Register</strong></a></br>
 
-<form name = "login" action= "login.php" method = "post">
-Username: <input type = "text" name = "username"/></br>
-Password: <input type = "password" name = "password"/></br>
-<input type = "submit" value = "Login" />
-</form>
-<a href = "register.php"><strong>Register</strong></a>
-
-<?php
-    session_start();  // creates session array(kinda like POST) that can save  variables between pages
-    if(isset($_SESSION['username'])){
-        echo  " Hello " . $_SESSION['username']; // use "." 
-?>
+            <?php
+            }
+        ?> 
+     </body>
+</html>
