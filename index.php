be8aee4168b73b1358c3e80cc4df1d3a9d1cd07e
mannyjl625 <mjl288@scarlemail.rutgers.edu>
2013-03-12 20:57:38 -0400

    pushing to new branch

--- index.php
+++ index.php
@@ -4,7 +4,11 @@
     <head>
         <title>Manny's Blog</title>
     </head>
-    <body>
+    <body>  
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="css/bootstrap.min,css" rel="stylesheet" media="screen">
+            <script src="http://code.jquery.com/jquery.js"></script>
+            <script src="js/bootstrap.min.js"></script>
             <?php
             session_start();  // creates session array(kinda like POST) that can save  variables between pages
             if(isset($_SESSION['username'])){
